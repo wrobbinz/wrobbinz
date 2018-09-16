566da7fc6fae60af6e06ce3f1d8d28ed4b60bb18
Will Robbins <will@sighten.io>
2018-09-16 13:51:24 -0700

    bring in unsaved tension

---
+++ src/MainView/Views/Tension.js
@@ -0,0 +1,27 @@
+import m from 'mithril';
+
+class Tension {
+  view() {
+    return (
+      <section id="main" class="full-height segment">
+        <div class="slide-up">
+          <h1>Tension</h1>
+          <p>
+            Robust note taking application supporting live markdown rendering.
+            Features search, tagging system, and favoriting notes.
+            </p>
+          <p>
+            <strong>STACK: </strong>React, Slate &mdash; Hapi, Knex, Objection &mdash; Postgres
+            </p>
+          <h2>DESIGN</h2>
+          <p>
+            Robust note taking application supporting live markdown rendering.
+            Features search, tagging system, and favoriting notes.
+            </p>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Tension;
